3c57c04947a72570546b95de1edc42fcde173f05
smuushi <mishih780@gmail.com>
2023-10-28 04:16:45 -0400

    work

--- src/components/watchdogeflag/WatchDogeFlagSection.jsx
+++ src/components/watchdogeflag/WatchDogeFlagSection.jsx
@@ -12,7 +12,7 @@ function WatchDogeFlagSection() {
             <div className="text-content">
                 <h2>代币经济模型</h2>
                 <p>
-                    在 “WatchDoge” 项目里, 我们的任务是经济模型与策略, ... (rest of the text)
+                    在 “WatchDoge” asdfasdfafdasfd, ... (rest of the text)
                 </p>
             </div>
         </div>
